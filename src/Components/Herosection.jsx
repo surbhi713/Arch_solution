a7ce35b211c4696243db4d0f2eabbcdc3064b7b3
surbhi713 <surbhi8004@gmail.com>
2025-06-26 10:55:28 +0530

    Update Herosection.jsx

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import img from '../assets/sbi borad.jpg';
+import img from '../assets/sbi borad.JPG';
 
 function Herosection() {
   useGSAP(() => {
